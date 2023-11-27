2dfc474addd6b9b17dc2f6ef2cb5ded330f0e6a9
UNKNOWN-MUSIC <130531308+KwsMusic@users.noreply.github.com>
2023-11-27 23:47:53 +0530

    Add files via upload

---
+++ __init__.py
@@ -0,0 +1,41 @@
+from AshishX.core.bot import AshishXBot
+from AshishX.core.dir import dirr
+from AshishX.core.git import git
+from AshishX.core.userbot import Userbot
+from AshishX.misc import dbb, heroku, sudo
+from aiohttp import ClientSession
+
+from .logging import LOGGER
+
+# Directories
+dirr()
+
+# Check Git Updates
+git()
+
+# Initialize Memory DB
+dbb()
+
+# Heroku APP
+heroku()
+
+# Load Sudo Users from DB
+sudo()
+
+# Bot Client
+app = AshishXBot()
+
+# Assistant Client
+userbot = Userbot()
+
+from .platforms import *
+
+YouTube = YouTubeAPI()
+Carbon = CarbonAPI()
+Spotify = SpotifyAPI()
+Apple = AppleAPI()
+Resso = RessoAPI()
+SoundCloud = SoundAPI()
+Telegram = TeleAPI()
+
+aiohttpsession = ClientSession()
